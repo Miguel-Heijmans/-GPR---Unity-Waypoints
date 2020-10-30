af37cb240d6d8d7cb5e272cf11b1cbe86ff54aca
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2020-10-30 18:55:32 +0100

    stopped the enemy from going at mach 100 in a random direction

--- Assets/Script/Health/Enemy.cs
+++ Assets/Script/Health/Enemy.cs
@@ -7,47 +7,34 @@ using static PlayerHealth;
 public class Enemy : HealthMain
 {
 
-    
-    //private Quaternion rotate;
-    [SerializeField] private GameObject test;
-    private Vector3 testLocation;
 
     public Slider healthBarSlider;
     public Text healthText;
 
-    void Awake()
-    {
-        //rotate = transform.rotation;
-        test = GameObject.FindGameObjectWithTag("Enemy");
-    }
+   
 
     void Update()
     {
-        //transform.rotation = rotate;
-        testLocation = test.transform.position;
-        this.gameObject.transform.position = new Vector3(testLocation.x, transform.position.y, testLocation.z + 1.5f);
-
-
-        // if (ene == 0)
-        // {
-        //     SceneManager.LoadScene(sceneBuildIndex: 1);
-        // }
+        
 
         healthBarSlider.maxValue = enemyMaxHp;
         healthBarSlider.value = enemyHp;
         healthText.text = healthBarSlider.value.ToString() + " / " + healthBarSlider.maxValue.ToString();
 
-       
-
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+        if (enemyHp <= 0)
         {
-            enemyHp -= 10;
-            // Debug.Log(enemyHp);
-            // Debug.Log("hp");
-            // Debug.Log(healthBarSlider.value);
-            // Debug.Log("slider");
-
+            Destroy(gameObject);
         }
+
+        // if (Input.GetKeyDown(KeyCode.Alpha5))
+        // {
+        //     enemyHp -= 10;
+        //     // Debug.Log(enemyHp);
+        //     // Debug.Log("hp");
+        //     // Debug.Log(healthBarSlider.value);
+        //     // Debug.Log("slider");
+
+        // }
     }
 }
 
